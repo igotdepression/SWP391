d65aae505c285201df846e21a5c032d18eed38ee
igotdepression <nguyenhoangminh21092004@gmail.com>
2025-06-06 05:45:38 +0700

    add and fix

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# SWP391
+# DNA Testing Service
+
+Dự án này cung cấp dịch vụ kiểm tra DNA với giao diện người dùng thân thiện và hệ thống backend mạnh mẽ.
--- frontend/bloodline-frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Filler from "./Filler";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Filler />);
